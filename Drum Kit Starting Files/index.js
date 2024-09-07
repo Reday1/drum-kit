926cf4b74e108664eaff20e25a4dcdebcfd209b6
Reday1 <155070198+Reday1@users.noreply.github.com>
2024-09-07 18:11:10 +0530

    Delete Drum Kit Starting Files directory

--- Drum Kit Starting Files/index.js
+++
@@ -1,7 +0,0 @@
-var numberDrums = document.querySelectorAll('.drum').length;
-for (var i; i<numberDrums; i++) {
-    document.querySelectorAll('.drum')[i].addEventListener('click', function(){
-        var audio = new Audio('sounds/crash.mp3');
-        audio.play();
-    });
-}
